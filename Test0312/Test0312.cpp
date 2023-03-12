ccb2e9f81e809b3e5d1e9df7fa302464b1d80c2e
BeauZX <rw13a1010@gmail.com>
2023-03-12 23:10:52 +0800

    輸出該月份的季節，判斷是哪個季節。

--- Test0312/Test0312.cpp
+++ Test0312/Test0312.cpp
@@ -1,24 +1,17 @@
-﻿#include <iostream>  
+﻿#include<iostream>  
 using namespace std;
 
 int main()
 {
-    int hore1, min1, hore2, min2;
+    int x = 0;
 
-    cin >> hore1;
-    cin >> min1;//開始時間  
-    cin >> hore2;
-    cin >> min2;//結束時間  
-
-    int time = (hore2 * 60 + min2) - (hore1 * 60 + min1);//計算總時數   
-    if (time <= 120 && time >= 0) {
-        cout << time / 30 * 30 << endl; 
-    }
-    else if (time > 120 && time <= 240) {
-        cout << (time - 120) / 30 * 40 + 120 << endl; 
-    }
-    else {
-        cout << (time - 240) / 30 * 60 + 120 + 160 << endl; 
+    cin >> x;
+    switch (x / 3)  
+    {
+    case 1:cout << "Spring" << endl; break;
+    case 2:cout << "Summer" << endl; break;
+    case 3:cout << "Autumn" << endl; break;
+    default:cout << "Winter" << endl;  
     }
     return 0;
 }

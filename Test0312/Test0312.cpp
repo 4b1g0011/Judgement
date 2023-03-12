676aec5cc9bd036305dad8b5b3931d90466d2c75
BeauZX <rw13a1010@gmail.com>
2023-03-12 22:57:36 +0800

    加入專案檔案。

---
+++ Test0312/Test0312.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>  
+using namespace std;
+
+int main()
+{
+    int x, y;
+    while (cin >> x >> y) {
+        int Test_radius = 10000;
+        double new_radius = (x * x) + (y * y); 
+        if (new_radius < Test_radius) {
+            cout << "inside\n";
+        }
+        else {
+            cout << "outside\n";
+        }
+    }
+    return 0;
+}
